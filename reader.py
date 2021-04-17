114e5281401159b61585421a28136d2fee5c0521
JudePark96 <judepark@kookmin.ac.kr>
2021-04-18 04:20:16 +0900

    add: extract title inside a tag

--- reader.py
+++ reader.py
@@ -38,6 +38,10 @@ class ReaderBase(object):
         items = re.findall(r'(?<=&lt;a href=")[^"]*', text)
         return [unquote(item) for item in items] if is_decode else items
 
+    def extract_hyper_link_as_title(self, text: str) -> List[str]:
+        text = text.replace('&lt;', '<').replace('&gt;', '>')
+        return re.findall(r'[^>]*>*([^<]+)</a>', text)
+
     def clean_markup(self, text: str) -> str:
         text = text.replace('&lt;', '<').replace('&gt;', '>')
         return re.sub(r'<.*?>', '', text)
@@ -57,6 +61,7 @@ class WikiReader(ReaderBase):
     def construct_entity_graph(self):
         target_files = self.aggregate_files_in_folders()
         output = []
+        c = re.compile(r'[^ .,?!/@$%~％·∼()\x00-\x7F가-힣]+')
         for file in tqdm(target_files, desc='target files loading'):
             json_data = self.read_json_file(file)
             for json_object in json_data:
@@ -64,21 +69,34 @@ class WikiReader(ReaderBase):
                     'cur_id': json_object['id'],
                     'revid': json_object['revid'],
                     'paragraphs': [
-
                     ]
                 }
                 paragraphs = self.split_paragraph(json_object['text'])
 
                 for paragraph in paragraphs:
                     clean_text = self.clean_markup(paragraph)
+                    if clean_text == '':
+                        continue
+
                     hyperlink = self.extract_hyper_link(paragraph)
 
+                    if len(hyperlink) == 0:
+                        continue
+
+                    hyperlink_title = self.extract_hyper_link_as_title(paragraph)
+
+                    # this shouldn't be the zero!
+                    assert len(hyperlink_title) != 0
+                    entity_spans = hyperlink_title
+
                     paragraph_dict = {
-                        'text': clean_text,
-                        'cur_id_by_name': [link for link in hyperlink]
+                        'text': re.sub(c, '', clean_text),
+                        'cur_id_by_name': [link for link in hyperlink],
+                        'entity_span': entity_spans
                     }
                     article_dict['paragraphs'].append(paragraph_dict)
-                output.append(article_dict)
+                if len(article_dict['paragraphs']) != 0:
+                    output.append(article_dict)
 
         print('saving start!')
         with open(self.out_path, 'wb') as f:
@@ -90,24 +108,3 @@ class WikiReader(ReaderBase):
 if __name__ == '__main__':
     reader_base = WikiReader(path='./rsc/wikipedia/text', out_path='./rsc/wikipedia/output.pkl')
     reader_base.construct_entity_graph()
-    print(reader_base.get_cur_id('노벨 평화상'))
-    targets = reader_base.aggregate_files_in_folders()
-    json_data = reader_base.read_json_file(targets[0])[0]
-
-    # it only considers first paragraph of fetched article.
-    text = json_data['text'].split('\n')[0]
-    # print(text)
-    # print(reader_base.clean_markup(text))
-    # print(re.findall(r'(?<=&lt;a href=")[^"]*', text))
-    # print(reader_base.extract_hyper_link(text))
-
-
-
-
-    """
-    제임스 얼 카터 주니어(, &lt;a href="1924%EB%85%84"&gt;1924년&lt;/a&gt; &lt;a href="10%EC%9B%94%201%EC%9D%BC"&gt;10월 1일&lt;/a&gt; ~ )는 &lt;a href="%EB%AF%BC%EC%A3%BC%EB%8B%B9%20%28%EB%AF%B8%EA%B5%AD%29"&gt;민주당&lt;/a&gt; 출신 &lt;a href="%EB%AF%B8%EA%B5%AD"&gt;미국&lt;/a&gt; 39대 대통령 (&lt;a href="1977%EB%85%84"&gt;1977년&lt;/a&gt; ~ &lt;a href="1981%EB%85%84"&gt;1981년&lt;/a&gt;)이다.
-    제임스 얼 카터 주니어(, 1924년 10월 1일 ~ )는 민주당 출신 미국 39대 대통령 (1977년 ~ 1981년)이다.
-    ['1924%EB%85%84', '10%EC%9B%94%201%EC%9D%BC', '%EB%AF%BC%EC%A3%BC%EB%8B%B9%20%28%EB%AF%B8%EA%B5%AD%29', '%EB%AF%B8%EA%B5%AD', '1977%EB%85%84', '1981%EB%85%84']
-    ['1924년', '10월 1일', '민주당 (미국)', '미국', '1977년', '1981년']
-    """
-
